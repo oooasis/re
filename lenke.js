2e34ba1fcb4bfc4d52e3e918a7949dbb48131c48
oooasis <39504742+oooasis@users.noreply.github.com>
2025-10-31 11:54:37 +0800

    Create lenke.js

---
+++ lenke.js
@@ -0,0 +1,13 @@
+let body = {
+  "code" : "0",
+  "data" : {
+    "SecurityToken" : "CAIS4gJ1q6Ft5B2yfSjIr5nDDe7AhqZV9fG7W1TWjkdhY/55tojlhTz2IHFPfXJuB+8bvvU0mGxW6foZlqp6U4cd1vMKxgArvPpt6gqET9fruq7ctM4I6vCMHWyUFGS9vqv7aPn4S9XwY+qkb0u++AZ43br9c0fcPSChNpLbsZl8cvAQUTKkaiBLINBZPXEhyqkgOGDWKOymPzPzn2PUFzAIgAdnjn5l4qnNpa/54xHF3ln9xb1X9cajYLrcNpk8YMclAorth70mKPuQixQ9sUYaqP1E64Vf4irCs92nBF1c3g6LKeK88Kc0fVQgPfZkRPMU/aOgyaMn5rfJ643zygdQO/pRSCjSSom9xNfeH+ekJg/rrVZylLCmOLmtQXqV22tMCRpzXIja7lmdg9LeI6iNW+N3W062eSErkz3ZP4yv+o+4v5S7gatGc3TE2aYB7TvnL0R8GNsIC2lMUbp+w6lsKPFuG2QagAFi2l48iDKp5j72pV9gsfbxdIrf7AzM5T0TASOwCJlXmsS1kqf93iiPgBFYyWKIJwBs5qi8VWsAkVjaWWDAp7OBV631TxQvwHl0dWBRn4kqsQn6LNY4ugtVMD6e5XDqnJb0dt2OXFd4BMTOw7arh5Uv2umSpNmX8UBSTMLAc7QTWSAA",
+    "Expiration" : 1761882613,
+    "AccessKeySecret" : "49pidbHNxSZMk8bMDPqgy5mKySrJ6M4JWKgRFpBUZeRQ",
+    "AccessKeyId" : "STS.NZvFTtkytB3PYrgjG4oqUYMNg"
+  },
+  "msg" : "success",
+  "act" : 0
+};
+
+$done({ body: JSON.stringify(body) });
